38c7b7d0f7b0e49517c6eda3246ab3ed46464b33
Kamran <mohammadkamran615@gmail.com>
2026-03-11 10:31:10 +0530

    updated backend url for production

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://attendance-system-8x89.onrender.com",
+  withCredentials: true
 });
 
 // attach token automatically
