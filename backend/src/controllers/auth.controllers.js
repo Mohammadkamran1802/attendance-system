2b54fb727f14a47b1815ef3fc0b4a865298814b3
Kamran <mohammadkamran615@gmail.com>
2026-03-14 09:15:41 +0530

    fixed token

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -40,7 +40,7 @@ export const login = async (req, res) => {
             return res.status(400).json({message: "Invalid email or password"});
         }
         const token = jwt.sign(
-            {id: user._id, role: user.role},
+            {id: user.id, role: user.role},
             process.env.JWT_SECRET,
             {expiresIn: "1d"}
 
