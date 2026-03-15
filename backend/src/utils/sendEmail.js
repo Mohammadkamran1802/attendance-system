419700ac67af837fecb06713ce85fb88a246a56e
Kamran <mohammadkamran615@gmail.com>
2026-03-15 15:18:53 +0530

    nodemaliler fixed

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,7 +4,7 @@ const sendEmail = async ({ to, subject, text }) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, // app password
